28a556dfd74c813be509d3fca681b177ac5cd805
Anil <t3shan00@students.oamk.fi>
2024-11-10 00:51:41 +0200

    part 5

---
+++ server/routes/todoRouter.js
@@ -0,0 +1,39 @@
+import { pool } from '../helper/db.js'
+import { Router } from 'express'
+import { empty0rRows } from '../helper/util.js'
+
+const router = Router()
+
+router.get('/', (req,res) => {
+    pool.query('select * from task',(error, result) => {
+        if (error) {
+            return next(error)
+        }
+        return res.status(200).json(empty0rRows(result))
+    })
+})
+
+router.post('/create', (req,res) => {
+    pool.query('insert into task (description) values ($1) returning *', [req.body.description],
+        (error,result) => {
+            if (error) {
+                return next(error)
+            }
+            return res.status(200).json(empty0rRows(result))
+        }
+    )
+})
+
+router.delete('/delete/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    pool.query('delete from task where id = $1', [id],
+        (error,result) => {
+            if (error) {
+                return next(error)
+            }
+            return res.status(200).json(empty0rRows(result))
+        }
+    )
+})
+
+export { router as todoRouter }
